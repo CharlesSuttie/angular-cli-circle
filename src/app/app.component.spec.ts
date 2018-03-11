fe7fdb13ae3eaae76b7b4f5864c5c17c0f23ab95
Charles Suttie <charles@charlessuttie.com>
2018-03-11 12:22:22 +1100

    Update angular 5.2.0 => 5.2.8 Flip tests to passing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,8 +16,8 @@ describe('AppComponent', () => {
   it(`should have as title 'app'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    // expect(app.title).toEqual('app');
-    expect(app.title).toEqual('incorrect');
+    expect(app.title).toEqual('app');
+    // expect(app.title).toEqual('incorrect');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);

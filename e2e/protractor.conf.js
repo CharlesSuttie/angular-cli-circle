0967ad689c3dffc3a99d0fbec8b703e9ce4366ed
Charles Suttie <charles@charlessuttie.com>
2018-05-20 18:29:18 +1000

    Add circle test configuration Add junit reporting to e2e test configuration

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -2,6 +2,7 @@
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
 const { SpecReporter } = require('jasmine-spec-reporter');
+const jasmineReporters = require('jasmine-reporters');
 
 exports.config = {
   allScriptsTimeout: 11000,
@@ -24,5 +25,9 @@ exports.config = {
       project: require('path').join(__dirname, './tsconfig.e2e.json')
     });
     jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+    jasmine.getEnv().addReporter(new jasmineReporters.JUnitXmlReporter({
+      consolidateAll:true,
+      filePrefix: 'e2e'
+    }));
   }
 };

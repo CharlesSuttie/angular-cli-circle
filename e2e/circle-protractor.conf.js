547409458c9255cc798d8f34494d8b4bf51017e8
Charles Suttie <charles@charlessuttie.com>
2018-07-01 21:20:46 +1000

    Refactor protractor configuration file

--- e2e/circle-protractor.conf.js
+++ e2e/circle-protractor.conf.js
@@ -1,23 +1,11 @@
 const jasmineReporters = require('jasmine-reporters');
 
-exports.config = {
-  allScriptsTimeout: 11000,
-  specs: [
-    './src/**/*.e2e-spec.ts'
-  ],
-  capabilities: {
-    'browserName': 'chrome'
-  },
+const config = require('./protractor.conf').config;
+
+config.capabilities = {
+  'browserName': 'chrome',
   chromeOptions: {
     args: [ '--headless', '--disable-gpu' ]
-  },
-  directConnect: true,
-  baseUrl: 'http://localhost:4200/',
-  framework: 'jasmine',
-  jasmineNodeOpts: {
-    showColors: true,
-    defaultTimeoutInterval: 30000,
-    print: function() {}
   },
   onPrepare() {
     require('ts-node').register({
@@ -28,4 +16,6 @@ exports.config = {
       filePrefix: 'e2e'
     }));
   }
-}
+};
+
+exports.config = config;
